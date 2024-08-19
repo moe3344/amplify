4e6e7dcb1fb6b99ba104e9662413a8559aa8666f
moe3344 <mohamad.hammoud1@ucalgary.ca>
2024-08-19 01:52:08 -0600

    init

---
+++ my_amplify_app/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "amplifyeca2f133": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
